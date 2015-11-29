ddcf008e37bdd1f18b3871c5c842d52a4df52496
Annoraaq <baum@bw-webgestaltung.de>
2015-11-29 21:23:47 +0100

    Bugfix in insert script

--- measurements/TopoSortParser/src/toposortparser/TopoSortParser.java
+++ measurements/TopoSortParser/src/toposortparser/TopoSortParser.java
@@ -1,15 +1,24 @@
 package toposortparser;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author J. Baum
  */
 public class TopoSortParser {
+    
+    private static List<String> files = new ArrayList<>();
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        initFileList();
+        
         XmlParser xmlParser = new XmlParser();
         
 
@@ -18,12 +27,28 @@ public class TopoSortParser {
         dbManager.openConnection(true);
         dbManager.createDatabase();
         
-        for (Measurement temp : xmlParser.parseXml("results/resultfile.xml")) {
-            dbManager.insertMeasurement(temp);
+        for (String filename : files) {
+            System.out.println(filename);
+            for (Measurement temp : xmlParser.parseXml("results/" + filename)) {
+                dbManager.insertMeasurement(temp);
+            }
         }
         
         dbManager.closeConnection();
      
+    }
+    
+    private static void initFileList() {
+        
+        File folder = new File("results/");
+        File[] listOfFiles = folder.listFiles();
+        files = new ArrayList<>();
+
+        for (File file : listOfFiles) {
+            if (file.isFile() && file.getName().endsWith(".xml")) {
+                files.add(file.getName());
+            }
+        }
     }
 
 }
