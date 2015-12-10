39f25b0cb2be62d368950ef5562c6ee6fe90434b
wkevin <wkevin@student.ethz.ch>
2015-12-10 12:39:32 +0100

    split run script for euler

--- src/goeuler.sh
+++ src/goeuler.sh
@@ -1,2 +1,14 @@
+export OMP_NUM_THREADS=12
+bsub -n 12 ./runEulerParallel.sh 1 12
+
 export OMP_NUM_THREADS=24
-bsub -W 00:20 -n 24 ./runEuler.sh
+bsub -n 24 ./runEulerParallel.sh 12 24
+
+export OMP_NUM_THREADS=12
+bsub -n 12 ./runEulerPercentage.sh 1 12
+
+export OMP_NUM_THREADS=24
+bsub -n 24 ./runEulerPercentage.sh 12 24
+
+export OMP_NUM_THREADS=1
+bsub -n 1 ./runEulerSerial.sh
---
+++ src/runEulerPercentage.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# chmod +x run.sh
+
+#==================
+# GENERAL SETTINGS
+#==================
+# Number of threads
+THREADS_MIN=$1
+THREADS_STEP=1
+THREADS_MAX=$2
+
+# Number of repetitions for each experiment
+NREP=3
+
+# Executables
+declare -a PARALLEL_EXE=('./toposort_omp_bitset.exe' './toposort_omp_locallist.exe');
+SERIAL_EXE='./toposort_serial.exe'
+declare -a GRAPH_TYPES=('r' 's')
+#declare -a PARALLEL_EXE=('dryrun/test.exe')
+#SERIAL_EXE='dryrun/test.exe'
+
+# Problem sizes
+N_SSC=100000000    #10^8
+N_WSC=10000000  #10^7
+
+
+#=======================
+# PERCENTAGE BENCHMARKS
+#=======================
+ANALYSIS=1
+for (( i=0; i<$NREP; i=i+1 ))
+do
+	# Benchmark parallel versions
+	for op in {0..1}
+	do
+		make clean
+		make -j release OPT=$op AN=$ANALYSIS
+		for (( t=$THREADS_MIN; t<=$THREADS_MAX; t=t+$THREADS_STEP ))
+		do
+			export OMP_NUM_THREADS=$t
+			for v in "${PARALLEL_EXE[@]}"
+			do
+				for gt in "${GRAPH_TYPES[@]}"
+				do
+					# Strong scaling
+					eval "./$v $gt $N_SSC"
+				done
+			done    
+		done
+	done
+done
+
+
