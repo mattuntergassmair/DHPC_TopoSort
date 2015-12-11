c3506575008f0af4c3612cd11e20a071ce60ec0d
Matthias Untergassmair <untergam@stud.phys.ethz.ch>
2015-12-11 19:26:36 +0100

    sync_euler.sh now works - please adapt to fit your needs and then put in .gitignore

--- sync_euler.sh
+++ sync_euler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MYABSREPOPATH=/home/matthias/polybox/CSE7/DesignHPC/project/DHPC_TopoSort
-EULERREPOPATH=
+EULERREPOPATH=dphpc_toposort
 MYETHUSERNAME=untergam
 
 
@@ -9,11 +9,16 @@ MYETHUSERNAME=untergam
 #		-u	keep local files if newer than other file
 #		-z	compress files
 
+# You must have a ssh key registered on the cluster, check
+#		http://wacc.wisc.edu/docs/accessing/ --> Key-Based Authentication
+# 
+# No warranty here, but it works great for me
+# Use your own skript instead of this if you want
+
 # send source code to cluster
-echo "rsync -aiuz ${MYABSREPOPATH}/src/* ${MYETHUSERNAME}@euler.ethz.ch:dphpc_toposort/ --exclude '*.o' --exclude '*.exe' --exclude '*.xml' --exclude 'results/' --exclude 'graph_output/'"
+rsync -aiuz ${MYABSREPOPATH}/src/* ${MYETHUSERNAME}@euler.ethz.ch:dphpc_toposort/ --exclude '*.o' --exclude '*.exe' --exclude '*.xml' --exclude 'results/' --exclude 'graph_output/' --exclude 'dryrun/'
 
 # receive from cluster (include not strictly necessary, but more explicit)
-echo "rsync -aiuz ${MYETHUSERNAME}@euler.ethz.ch:dphpc_toposort/ ${MYABSREPOPATH}/src/* --exclude '*.o' --exclude '*.exe' --include '*.xml' --include 'results/' --include 'graph_output/'"
+rsync -aiuz ${MYETHUSERNAME}@euler.ethz.ch:dphpc_toposort/ ${MYABSREPOPATH}/src/* --exclude '*.o' --exclude '*.exe' --include '*.xml' --include 'results/' --include 'graph_output/' --exclude 'dryrun/'
 
 exit 0
-
