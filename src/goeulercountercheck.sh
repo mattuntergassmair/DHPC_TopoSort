882ae9bc97e167d49d4dc32fe10c6f4868d55212
wkevin <wkevin@student.ethz.ch>
2015-12-23 11:27:29 +0100

    Fix run scripts

--- src/goeulercountercheck.sh
+++ src/goeulercountercheck.sh
@@ -1,13 +1,13 @@
 module load boost
 
 export OMP_NUM_THREADS=12
-for $i in {0..2}
+for i in {0..2}
 do
 	bsub -n 12 ./runEulerCounterCheck.sh 1 12
 done
 
 export OMP_NUM_THREADS=24
-for $i in {0..2}
+for i in {0..2}
 do
 	bsub -n 24 ./runEulerCounterCheck.sh 13 24
 done
--- src/goeulerpushback.sh
+++ src/goeulerpushback.sh
@@ -1,13 +1,13 @@
 module load boost
 
 export OMP_NUM_THREADS=12
-for $i in {0..2}
+for i in {0..2}
 do
 	bsub -n 12 ./runEulerPushback.sh 1 12
 done
 
 export OMP_NUM_THREADS=24
-for $i in {0..2}
+for i in {0..2}
 do
 	bsub -n 24 ./runEulerPushback.sh 13 24
 done
