43663d59d9220628557b5c25a86ee66f2618353a
wkevin <wkevin@student.ethz.ch>
2016-01-16 15:39:31 +0100

    sugar for nodelete mode

--- measurements/createdb.sh
+++ measurements/createdb.sh
@@ -3,6 +3,9 @@
 # This is just a temporary helper script - should be made nicer
 
 cd TopoSortParser
-ant
-
+if [[ -z $1 && $1 != "--noDelete" ]]; then
+	ant
+else
+	ant run -Dargs="--noDelete"
+fi
 exit 0
